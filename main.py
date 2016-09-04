3680859c5e81a9aea186ebf31fd90e86361d45ec
Ananya Kirti <ananya.kirti@iitrpr.ac.in>
2016-09-05 00:14:33 +0530

    completed testing
    completed testing

--- main.py
+++ main.py
@@ -1,57 +1,128 @@
-import logging
+import operator
+from math import log
 
-
-class Instance(object):
-	"""This is the instance class used to collect the data."""
-	def __init__(self, inputString):
-		super(Instance, self).__init__()
-		self.email = inputString[0]
-		self.classLable = inputString[1]
-
-		
 
 def importData(file):
 	"""
 	 This function imports the data into a list form a file name passed as an argument. 
 	 The file should only the data seperated by a space.
 	"""
-	data = []
-	f = open(str(file), 'r')
-	for line in f:
-		current = line.split()	#enter your own delimiter like ","
-		data.append(Instance(current))
-	return data
+	hamData = {}
+	spamData = {}
+	vocabulary = {}
 
-
-def getPriorProbablities(data):
-	"""
-	Function to get the prior probablities of the dataset.
-	"""
 	ham = 0
 	spam = 0
-	for instances in data:
-		if instances.classLable == 'ham':
+
+	f = open(str(file), 'r')
+	for line in f:
+		current = line.split()
+		if current[1] == 'ham':
 			ham += 1
-		elif instances.classLable == 'spam':
-			spam +=1
-		else:
-			print "err"
-	ham =  float(ham)/len(data)
-	spam = float(spam)/len(data)
-	return ham, spam
+			for i in range(2, len(current), 2):
+				vocabulary[current[i]] = 1
+				if hamData.has_key(current[i]):
+					hamData[current[i]] = int(hamData.get(current[i])) + int(current[i+1])
+				else:
+					hamData[current[i]] = int(current[i+1])
 
-
-
-def main(FILENAME):
-	data = importData(FILENAME)
-	Pham, Pspam = getPriorProbablities(data)
+		elif current[1] == 'spam':
+			spam += 1
+			for i in range(2, len(current), 2):
+				vocabulary[current[i]] = 1
+				if spamData.has_key(current[i]):
+					spamData[current[i]] = int(spamData.get(current[i])) + int(current[i+1])
+				else:
+					spamData[current[i]] = int(current[i+1])
+	f.close()
 
 	
+	
+	Pham = float(ham) / (ham + spam)
+	Pspam = float(spam) / (ham + spam)
+	
+	print ham + spam
+	print Pham
+	print Pspam
 
+	return hamData, spamData, vocabulary, ham, spam
+
+
+def getConditionalProbablities(total, hamData, spamData):
+	hamProbablity ={}
+	spamProbablity ={}
+
+	totalHam = 0
+	totalSpam = 0
+
+	for word in hamData:
+		totalHam += hamData.get(word)
+
+	for word in spamData:
+		totalSpam += spamData.get(word)
+
+	for word in hamData:
+		hamProbablity[word] = float(hamData.get(word) + 1) / (totalHam + total)
+
+	for word in spamData:
+		spamProbablity[word] = float(spamData.get(word) + 1) / (totalSpam + total)
+
+	maxHam = dict(sorted(hamProbablity.iteritems(), key=operator.itemgetter(1), reverse=True)[:5])
+	maxSpam = dict(sorted(spamProbablity.iteritems(), key=operator.itemgetter(1), reverse=True)[:5])
+	print maxHam
+	print maxSpam
+
+	return hamProbablity, spamProbablity, totalHam, totalSpam
+
+
+def test(TESTNAME, hamProbablity, spamProbablity, totalHam, totalSpam, total):
+	correct = 0
+	testtotal = 0
+	f = open(str(TESTNAME), 'r')
+	for line in f:
+		spam = float(0)
+		ham = float(0)
+		current = line.split()
+		for i in range(2, len(current)):
+			# print hamProbablity.get(current[i], float(1) / (totalHam + total)),
+			# print log1p(hamProbablity.get(current[i], float(1) / (totalHam + total)))
+			# print spamProbablity.get(current[i], float(1) / (totalSpam + total)),
+			# print log1p(spamProbablity.get(current[i], float(1) / (totalSpam + total)))
+
+			ham += log(hamProbablity.get(current[i], float(1) / (totalHam + total)))
+			spam += log(spamProbablity.get(current[i], float(1) / (totalSpam + total)))
+			
+		print ham, spam, current[1]
+		
+		if ham > spam:
+			if current[1] == 'ham':
+				correct += 1
+		else:
+			if current[1] == 'spam':
+				correct += 1
+		testtotal += 1
+	f.close()
+
+	accuracy = float(correct) / testtotal * 100
+	return accuracy
+
+
+
+def main(FILENAME, TESTNAME):
+	hamData, spamData, vocabulary, ham, spam = importData(FILENAME)
+	total =  len(vocabulary.keys())
+	hamProbablity, spamProbablity, totalHam, totalSpam = getConditionalProbablities(total, hamData, spamData)
+
+	print test(TESTNAME, hamProbablity, spamProbablity, totalHam, totalSpam, total)
+
+
+
+	
 
 if __name__ == '__main__':
 	"""
 	Main driver function for the experiment.
 	"""
-	FILENAME = 'nbctest'
-	main(FILENAME)
+	FILENAME = 'nbctrain'
+	TESTNAME = 'nbctest'
+	main(FILENAME, TESTNAME)
